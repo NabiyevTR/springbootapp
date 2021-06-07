d75528792c6db0a765e260d6a08d1b0bbe0ab2af
Timur Nabiev <NabiyevTR@gmail.com>
2021-06-07 11:00:06 +0300

    Update hiberate config file

--- src/main/java/ru/ntr/springbootapp/SpringBootApp.java
+++ src/main/java/ru/ntr/springbootapp/SpringBootApp.java
@@ -1,25 +1,17 @@
 package ru.ntr.springbootapp;
 
-import org.hibernate.cfg.Configuration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 
 @SpringBootApplication
 public class SpringBootApp {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApp.class, args);
-
-        EntityManagerFactory factory = new Configuration()
-                .configure("hibernate.cfg.xml")
-                .buildSessionFactory();
-
-        EntityManager manager = factory.createEntityManager();
     }
 
     @Bean
